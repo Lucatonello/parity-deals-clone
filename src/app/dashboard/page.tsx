184f2e832a209b1261ab1ef6a30f2078003ea2f8
Lucatonello <lucatonellow@gmail.com>
2024-12-12 20:10:13 -0300

    tried to fix userid from auth function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,33 @@
+import { getProducts } from '@/server/db/products';
+import { useAuth } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
+
 export default function DashboardPage() {
-    return null
+    const { userId, isLoaded, isSignedIn } = useAuth();
+    const [products, setProducts] = useState<any>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!isLoaded) return;
+
+        if (!isSignedIn) {
+            setError('User is not signed in');
+            return;
+        }
+
+
+        const fetchProducts = async () => {
+            const response = await getProducts(userId, { limit: 6 });
+            setProducts(response);
+        }
+
+        fetchProducts();
+        console.log('User ID:', userId, 'Products: ', products);
+    }, [isLoaded, isSignedIn, userId, products]);
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return <div></div>;
 }
