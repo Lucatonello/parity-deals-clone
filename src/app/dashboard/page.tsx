8040399e94557657dc96093690f658ac14a5043a
Lucatonello <lucatonellow@gmail.com>
2024-12-12 21:34:21 -0300

    Fixed auth function issue--properly handle userId

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,33 +1,15 @@
-import { getProducts } from '@/server/db/products';
-import { useAuth } from '@clerk/nextjs';
-import { useEffect, useState } from 'react';
+import { auth } from '@clerk/nextjs/server'
+import { getProducts } from '@/server/db/products'
+import { NoProducts } from './_components/NoProducts'
 
-export default function DashboardPage() {
-    const { userId, isLoaded, isSignedIn } = useAuth();
-    const [products, setProducts] = useState<any>([]);
-    const [error, setError] = useState<string | null>(null);
+export default async function Page() {
+    const { userId }: { userId: string | null } = await auth()
 
-    useEffect(() => {
-        if (!isLoaded) return;
+    if (!userId) return null
 
-        if (!isSignedIn) {
-            setError('User is not signed in');
-            return;
-        }
+    const products = await getProducts(userId, { limit: 6 })
 
+    if (products.length === 0) return <NoProducts />
 
-        const fetchProducts = async () => {
-            const response = await getProducts(userId, { limit: 6 });
-            setProducts(response);
-        }
-
-        fetchProducts();
-        console.log('User ID:', userId, 'Products: ', products);
-    }, [isLoaded, isSignedIn, userId, products]);
-
-    if (error) {
-        return <div>{error}</div>;
-    }
-
-    return <div></div>;
+    return <h1>Hello, {userId}</h1>
 }
--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,6 +1,7 @@
 import { db } from "@/drizzle/db";
 
 export function getProducts(userId: string, { limit }: { limit?: number} ) {
+    console.log('userId in getProducts function: ', userId);
     return db.query.ProductTable.findMany({
         where: (({ clerkUserId }, {eq}) => eq(clerkUserId, userId)),
         orderBy: ({ createdAt }, {desc}) => desc(createdAt),
